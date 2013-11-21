7b98b63a534444b299d65d9530d0883cbdbd32f8
xiaojue <designsor@gmail.com>
2013-11-21 17:55:34 +0800

    add play and stop func

--- public/bootstrap/js/weiboting.js
+++ public/bootstrap/js/weiboting.js
@@ -19,7 +19,7 @@
 			url: url,
 			data: {
 				count: 20,
-                feature:1,
+				feature: 1,
 				page: page
 			},
 			dataType: 'json',
@@ -29,10 +29,28 @@
 		});
 	}
 
+	var regUrl = /(htt(p|ps):\/\/[A-Za-z\.\d\/\?\&\;\=\-\#_(\u4e00-\u9fa5)]+)/gi;
+
+	function stripscript(s) {
+		var pattern = new RegExp("[`~!#$^&*()=|{}':;',\\[\\].<>/?~！#￥……&*（）——|{}【】‘“'。、]");
+		var rs = "";
+		for (var i = 0; i < s.length; i++) {
+			rs = rs + s.substr(i, 1).replace(pattern, ' ');
+		}
+		return rs;
+	}
+
+	function fixText(txt) {
+		txt = txt.replace(regUrl, '');
+		txt = txt.replace(/\[*.\]/g, '');
+		txt = stripscript(txt);
+		return txt;
+	}
+
 	function appendContent(items) {
 		var html = '<ol class="feeds">';
 		items.forEach(function(item) {
-			html += '<li><span class="text-info">' + item.user.name + '</span> 说: '+ item.text + '</li>';
+			html += '<li><span class="text-info">' + item.user.name + '</span> 说: ' + fixText(item.text) + '</li>';
 		});
 		html += '</ol>';
 		contentWrap.html(html);
@@ -41,19 +59,48 @@
 	function home_line() {
 		$('[node-type=home_line]').parent().addClass('active');
 		get_line('/home_list', 1, function(json) {
-            appendContent(json.items);
+			appendContent(json.items);
 		});
 	}
 
 	function public_line() {
 		$('[node-type=public_line]').parent().addClass('active');
 		get_line('/public_list', 1, function(json) {
-            appendContent(json.items);
-        });
+			appendContent(json.items);
+		});
 	}
 
 	bind();
 
 	$(window).bind('hashchange', bind);
+
+	$('[node-type=stop]').addClass('disabled');
+
+	function play(item) {
+        var msg = contentWrap.find('li:eq('+item+')').text();
+		try {
+			var speech = new SpeechSynthesisUtterance(msg);
+			speech.lang = "zh-CN";
+			speechSynthesis.speak(speech);
+		} catch(e) {
+			alert(e);
+		}
+	}
+
+	function stop() {
+
+	}
+
+	$('[node-type=start]').click(function() {
+		$(this).addClass('disabled');
+		$('[node-type=stop]').removeClass('disabled');
+		play(0);
+	});
+	$('[node-type=stop]').click(function() {
+		$(this).addClass('disabled');
+		$('[node-type=start]').removeClass('disabled');
+		stop();
+	});
+
 })();
 
